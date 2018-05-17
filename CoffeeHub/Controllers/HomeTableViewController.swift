832410c5883dc77ce9819eede1d88ddd0afe824b
jullynobre <jullynobre1@gmail.com>
2018-05-17 09:45:15 -0300

    Add tableView with predefined cells

--- CoffeeHub/Controllers/HomeTableViewController.swift
+++ CoffeeHub/Controllers/HomeTableViewController.swift
@@ -9,15 +9,14 @@
 import UIKit
 
 class HomeTableViewController: UITableViewController {
-
+    
+    var recipes: [Recipe] = []
+    var grains: [Grain] = []
+    var favorites: [Coffee] = [Grain.init(name: "Arábica", icon: #imageLiteral(resourceName: "finalLogo"), difficulty: Difficulty.easy, prepTime: Float(20.9), typeDescription: "Lalala")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,16 +27,15 @@ class HomeTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 3
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
@@ -45,7 +43,7 @@ class HomeTableViewController: UITableViewController {
 
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
