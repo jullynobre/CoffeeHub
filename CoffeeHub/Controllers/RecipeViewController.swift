0d151db0ad6284bf36d8ba978b8a2f16f4f5a4ac
jullynobre <jullynobre1@gmail.com>
2018-05-18 12:04:37 -0300

    Add Recipe icons

---
+++ CoffeeHub/Controllers/RecipeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RecipeViewController.swift
+//  CoffeeHub
+//
+//  Created by Ada 2018 on 18/05/18.
+//  Copyright © 2018 Academy 2018. All rights reserved.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
